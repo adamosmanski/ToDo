e4c02e94f336663039f17cf7dddfd20d03d4fbb3
Adam Osmański <adamosmanski1@gmail.com>
2025-10-23 18:24:10 +0200

    Init project API setup

---
+++ ToDo.Backend/ToDo.Backend.API/Controllers/ToDoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo.Backend.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ToDoController : ControllerBase
+    {
+        public ToDoController()
+        {
+
+        }
+    }
+}
---
+++ ToDo.Backend/ToDo.Backend.API/Interface/IToDoService.cs
@@ -0,0 +1,13 @@
+﻿using ToDo.Backend.API.Models;
+
+namespace ToDo.Backend.API.Interface
+{
+    public interface IToDoService
+    {
+        Task<IEnumerable<ToDoItem>> GetAllAsync();
+        Task<ToDoItem?> GetByIdAsync(int id);
+        Task AddAsync(ToDoItem item);
+        Task UpdateAsync(ToDoItem item);
+        Task SaveChangesAsync();
+    }
+}
