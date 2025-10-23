36d28e098fbeaab45f032743908fe9b6877cbb2d
Adam Osmański <adamosmanski1@gmail.com>
2025-10-23 21:42:00 +0200

    Init Database config

--- ToDo.Backend/ToDo.Backend.API/Program.cs
+++ ToDo.Backend/ToDo.Backend.API/Program.cs
@@ -1,6 +1,10 @@
 
+using Microsoft.EntityFrameworkCore;
 using ToDo.Backend.API.Interface;
 using ToDo.Backend.API.Serivce;
+using ToDo.Backend.Data;
+using ToDo.Backend.Data.Interface;
+using ToDo.Backend.Data.Repository;
 
 namespace ToDo.Backend.API
 {
@@ -16,7 +20,13 @@ namespace ToDo.Backend.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-            builder.Services.AddScoped<IToDoService, ToDoService>();
+
+            builder.Services.AddScoped<IToDoService, ToDoService>()
+                .AddScoped<IToDoRepository, ToDoRepository>();
+
+            builder.Services.AddDbContext<ToDoContext>(options =>
+               options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowAngularClient", policy =>
@@ -28,7 +38,11 @@ namespace ToDo.Backend.API
             });
             var app = builder.Build();
 
-
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ToDoContext>();
+                db.Database.Migrate();
+            }
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
---
+++ ToDo.Backend/ToDo.Backend.Data/ToDoContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.Backend.Data.Model;
+
+namespace ToDo.Backend.Data
+{
+    public class ToDoContext : DbContext
+    {
+        public ToDoContext(DbContextOptions<ToDoContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDoItem>(entity =>
+            {
+                entity.ToTable("todo_items");
+
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.Id)
+                      .HasColumnName("id")
+                      .ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Title)
+                      .HasColumnName("title")
+                      .HasMaxLength(200)
+                      .IsRequired();
+
+                entity.Property(e => e.Description)
+                      .HasColumnName("description")
+                      .HasMaxLength(1000);
+
+                entity.Property(e => e.IsCompleted)
+                      .HasColumnName("is_completed")
+                      .HasDefaultValue(false);
+
+                entity.Property(e => e.CreatedAt)
+                      .HasColumnName("created_at")
+                      .HasDefaultValueSql("CURRENT_TIMESTAMP");
+            });
+        }
+    }
+}
